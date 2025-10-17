5516bcf8660fb377309f9f8fd6cdb53f9910b2d5
Mauro Vargas <105677245+mvargas05@users.noreply.github.com>
2025-10-17 21:00:53 +0000

    Novo Header

--- controlefinanceiro/frontend/src/app/app.ts
+++ controlefinanceiro/frontend/src/app/app.ts
@@ -1,10 +1,14 @@
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from "./components/template/header/header";
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [Header],
   templateUrl: './app.html',
  
 })
