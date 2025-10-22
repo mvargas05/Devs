77f9434f4f9a5b949891060d15c39c692b8411b1
Mauro Vargas <105677245+mvargas05@users.noreply.github.com>
2025-10-22 00:15:45 -0300

    Melhorias de Navegação

--- controlefinanceiro/frontend/src/app/app.ts
+++ controlefinanceiro/frontend/src/app/app.ts
@@ -3,11 +3,14 @@ import { RouterOutlet } from '@angular/router';
 import { Header } from './components/template/header/header';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { Footer } from "./components/template/footer/footer";
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatListModule } from "@angular/material/list";
+import { Nav } from "./components/template/nav/nav";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [Header, MatToolbarModule, Footer],
+  imports: [Header, MatToolbarModule, Footer, MatSidenavModule, MatListModule, Nav],
   templateUrl: './app.component.html',
 })
 export class App {
